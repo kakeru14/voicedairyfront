838a128d02101feab95bc185d9426c13681d8f24
kakeru14 <kakeru.otani@rakus-partners.co.jp>
2021-07-26 10:28:26 +0900

    master

---
+++ src/views/Login .vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <h1>ログイン</h1>
+    <ui-textfield outlined v-model="state.name">
+    Your Name
+    </ui-textfield>
+    <ui-textfield
+    v-model="state.mail"
+  >
+    Email Address
+  </ui-textfield>
+</div>   
+</template>
+
+<script>
+import { defineComponent, reactive} from "vue";
+
+export default defineComponent({
+    name: "Login",
+    setup() {
+        const state = reactive({
+            name:"",
+            mail:""
+        })
+
+        return {state}
+    },
+})
+</script>

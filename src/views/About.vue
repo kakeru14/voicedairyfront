eaeca11af17daaeabc1e996abb7f6671855468c3
kakeru14 <kakeru.otani@rakus-partners.co.jp>
2021-08-03 17:21:51 +0900

    heroku

--- src/views/About.vue
+++ src/views/About.vue
@@ -101,7 +101,7 @@ import axios from "axios"
 import {useStore} from 'vuex'
 import {useRoute,useRouter} from 'vue-router'
 
-const baseURL = 'https://voicedairyfront.herokuapp.com'
+const baseURL = 'https://voicedairy.herokuapp.com'
 console.log('asasasa');
 // const deleteURL = 'http://localhost'
 
